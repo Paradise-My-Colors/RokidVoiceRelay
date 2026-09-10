0a390af7cef806d6804baf5e30890f9f9483c5d3
Paradise-My-Colors <fardos4ever@gmail.com>
2026-09-10 20:23:02 +0200

    v0.4: bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.paradisemc.rokid.plugin.voicerelay"
         minSdk = 30
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 
     compileOptions {
